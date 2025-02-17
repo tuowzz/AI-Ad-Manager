af9baece4f0200df3e45f56ee7606b544a609368
tuowzz <tuowtest006@hotmail.com>
2025-02-17 12:33:25 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+# โหลด API Key จาก .env
+load_dotenv()
+ACCESS_TOKEN = os.getenv("FACEBOOK_ACCESS_TOKEN")
+AD_ACCOUNT_ID = os.getenv("AD_ACCOUNT_ID")
+
+app = Flask(__name__)
+
+# ดึงข้อมูลจาก Facebook Ads API
+@app.route('/get_ads', methods=['GET'])
+def get_ads():
+    url = f"https://graph.facebook.com/v18.0/{AD_ACCOUNT_ID}/insights"
+    params = {
+        "fields": "impressions,clicks,cost_per_click",
+        "access_token": ACCESS_TOKEN
+    }
+    response = requests.get(url, params=params)
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True)
